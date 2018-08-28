d9f0c595411f561aa21535b2bb3ef20cefa2daac
David Xia <david.xia83@gmai.com>
2018-08-28 23:54:45 +1000

    Updated to make promo part of list

--- messenger-api-helpers/messenger-setup.js
+++ messenger-api-helpers/messenger-setup.js
@@ -27,7 +27,7 @@ const setHomeUrl = () => {
   api.callMessengerProfileAPI(
     {
       home_url: {
-        url: [APP_URL],
+        url: [APP_URL]+'lists/new',
         webview_height_ratio: 'tall',
         webview_share_button: 'show',
         in_test:true
