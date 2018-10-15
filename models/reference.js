11485ba568c4e27415dfdb8316ea7e638c972fe2
David Xia <david.xia83@gmai.com>
2018-10-15 13:07:20 +1100

    new build

---
+++ models/reference.js
@@ -0,0 +1,61 @@
+// ===== DB ====================================================================
+import Knex  from '../db/knex';
+
+const Country = () => Knex('country');
+//const City = () => Knex('city');
+//const Attraction = () => Knex('attraction');
+
+// ===== Region & Country ======================================================
+const getAllRegion = () =>
+  Country()
+    .distinct('region')
+    .select();
+
+const getAllCountry = () =>
+  Country()
+    .select('id as countryId', 'name as countryName', 'desc as countryDesc');
+
+const getCountryByRegion = (region) =>
+  Country()
+    .select('id as countryId', 'name as countryName', 'desc as countryDesc')
+    .where('region', region)
+    .orWhere('alias', 'like', '%'+region+'%')
+    .orWhere('tag', 'like', '%'+region+'%');
+
+const getCountry = (countryId) =>
+  Country()
+    .select('id as countryId', 'name as countryName', 'desc as countryDesc')
+    .where('id', countryId);
+
+const setCountry = (country) =>
+  Country()
+    .where({id: country.id})
+    .update(
+      {
+        name: country.name,
+        region: country.region,
+        desc: country.desc,
+        tag: country.tag,
+        alias: country.alias,
+      },
+      ['id', 'name', 'region', 'desc', 'tag', 'alias']
+    );
+
+const addCountry = (country) =>
+  Country()
+    .insert(country, ['id', 'name', 'region', 'desc', 'tag', 'alias']);
+
+const delCountry = (countryId) =>
+  Country()
+    .where('id', countryId)
+    .del();
+
+export default {
+  getAllRegion,
+  getAllCountry,
+  getCountryByRegion,
+  getCountry,
+  setCountry,
+  addCountry,
+  delCountry,
+};
---
+++ routes/country.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import Ref from '../models/reference';
+
+const router = express.Router();
+
+router.get('/', function(req, res) {
+  console.log('>>>>Retrieve all country items');
+  Ref.getAllCountry()
+    .then((result) => {
+      console.log('>>>>Retrieved all country items', result);
+      res.send(result);
+    })
+});
+
+router.get('/:countryId', function(req, res) {
+  const countryId = req.params.countryId;
+  console.log('>>>>Retrieve country by countryId', countryId);
+  Ref.getCountry(countryId)
+    .then((result) => {
+      console.log('>>>>Retrieved country item', result);
+      res.send(result);
+    })
+});
+
+router.put('/', function(req, res) {
+  const country = req.body;
+  console.log('>>>>Insert country item', country);
+
+  Ref.addCountry(country)
+    .then(([result]) => {
+      console.log('>>>>Inserted country item', result);
+      res.send(result);
+    })
+});
+
+router.post('/', function(req, res) {
+  const country = req.body;
+  console.log('>>>>Update country item', country);
+
+  Ref.setCountry(country)
+    .then(([result]) => {
+      console.log('>>>>Updated country item', result);
+      res.send(result);
+    })
+});
+
+router.delete('/', function(req, res) {
+  const item = req.body;
+  console.log('>>>>Delete reference item', item);
+
+  Ref.delReference(item.id)
+    .then(() => {
+      console.log('>>>>Deleted reference item', item);
+      res.send('ok');
+    })
+});
+
+export default router;
