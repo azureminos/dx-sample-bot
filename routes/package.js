db53dc01fe28f36db2991434a24aa85d5bb1db7c
David Xia <apolo2david@yahoo.com>
2018-10-17 12:20:42 +1100

    Update package.js

--- routes/package.js
+++ routes/package.js
@@ -5,23 +5,29 @@ const router = express.Router();
 
 router.get('/', function(req, res) {
   const query = req.body;
-  console.log('>>>>Retrieve city', query);
+  console.log('>>>>Retrieve package', query);
 
   if(query) {
-    if(query.countryId) {
-      Ref.getCityByCountryId()
+    if(query.countryName) {
+      Ref.getPackageByCountry()
         .then((result) => {
-          console.log('>>>>Retrieved all city items', result);
+          console.log('>>>>Retrieved all package items by country['+query.countryName+']', result);
           res.send(result);
         });
-    } else if(query.countryName) {
-      Ref.getCityByCountryName()
+    } else if(query.cityName) {
+      Ref.getPackageByCity()
         .then((result) => {
-          console.log('>>>>Retrieved all city items', result);
+          console.log('>>>>Retrieved all package items by city['+query.cityName+']', result);
+          res.send(result);
+        });
+    } else if(query.isPromoted) {
+      Ref.getAllPromotedPackage()
+        .then((result) => {
+          console.log('>>>>Retrieved all promoted package items', result);
           res.send(result);
         });
     } else {
-      Ref.getAllCity()
+      Ref.getAllPackage()
         .then((result) => {
           console.log('>>>>Retrieved all city items', result);
           res.send(result);
@@ -36,45 +42,45 @@ router.get('/', function(req, res) {
   }
 });
 
-router.get('/:cityId', function(req, res) {
-  const cityId = req.params.cityId;
-  console.log('>>>>Retrieve city by cityId', cityId);
-  Ref.getCity(cityId)
+router.get('/:packageId', function(req, res) {
+  const packageId = req.params.packageId;
+  console.log('>>>>Retrieve package by packageId', packageId);
+  Ref.getPackage(packageId)
     .then((result) => {
-      console.log('>>>>Retrieved city item', result);
+      console.log('>>>>Retrieved package item', result);
       res.send(result);
     })
 });
 
 router.put('/', function(req, res) {
-  const city = req.body;
-  console.log('>>>>Insert city item', city);
+  const package = req.body;
+  console.log('>>>>Insert package item', package);
 
-  Ref.addCity(city)
+  Ref.addPackage(package)
     .then(([result]) => {
-      console.log('>>>>Inserted city item', result);
+      console.log('>>>>Inserted package item', result);
       res.send(result);
     })
 });
 
 router.post('/', function(req, res) {
-  const city = req.body;
-  console.log('>>>>Update city item', city);
+  const package = req.body;
+  console.log('>>>>Update package item', package);
 
-  Ref.setCity(city)
+  Ref.setPackage(package)
     .then(([result]) => {
-      console.log('>>>>Updated city item', result);
+      console.log('>>>>Updated package item', result);
       res.send(result);
     })
 });
 
 router.delete('/', function(req, res) {
-  const city = req.body;
-  console.log('>>>>Delete city item', city);
+  const package = req.body;
+  console.log('>>>>Delete package item', package);
 
-  Ref.delCity(city.id)
+  Ref.delPackage(package.id)
     .then(() => {
-      console.log('>>>>Deleted city item', city);
+      console.log('>>>>Deleted package item', package);
       res.send('ok');
     })
 });
