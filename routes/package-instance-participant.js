4555ce95abc6d1d5aa744ed2a9a5c22c4c9f2c13
David Xia <apolo2david@yahoo.com>
2018-10-25 13:52:38 +1100

    Create package-instance-participant.js

---
+++ routes/package-instance-participant.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import InstanceParticipants from '../models/package-instance-participant';
+
+const router = express.Router();
+
+router.get('/:instId', function(req, res) {
+  const instId = req.params.instId;
+  console.log('>>>>Retrieve package-item by itemId', itemId);
+  InstanceParticipants.getParticipantByInstId(instId)
+    .then((result) => {
+      console.log('>>>>Retrieved package-item participant', result);
+      res.send(result);
+    })
+});*/
+
+router.put('/', function(req, res) {
+  console.log('>>>>Insert package-instance participant', req.body);
+  const {instId, userId} = req.body;
+
+  InstanceParticipants.addParticipant(instId, userId)
+    .then((result) => {
+      console.log('>>>>Inserted package-instance participant', result);
+      res.send(result);
+    })
+});
+
+/*router.post('/', function(req, res) {
+  const item = req.body;
+  console.log('>>>>Update package-item item', item);
+
+  PackageItem.setPackageItem(item)
+    .then(([result]) => {
+      console.log('>>>>Updated package-item item', result);
+      res.send(result);
+    })
+});*/
+
+router.delete('/', function(req, res) {
+  console.log('>>>>Delete package-instance participant', req.body);
+  const {InstId} = req.body;
+
+  InstanceParticipants.delParticipant(InstId)
+    .then(() => {
+      console.log('>>>>Deleted package-instance participant for instance['+InstId+']');
+      res.send('ok');
+    })
+});
+
+export default router;
