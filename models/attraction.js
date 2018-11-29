4ef904f5da1f11923ab3830aad81b37e5da360c5
David Xia <apolo2david@yahoo.com>
2018-11-29 20:44:12 +1100

    Update attraction.js

--- models/attraction.js
+++ models/attraction.js
@@ -6,22 +6,37 @@ const Attraction = () => Knex('attraction');
 // ===== Attraction ======================================================
 const getAllAttraction = () =>
   Attraction()
-    .join('city', {'attraction.city_id': 'city.id'})
-    .select('attraction.id', 'attraction.name', 'attraction.city_id as cityId','city.name as cityName',
-      'attraction.description', 'attraction.alias', 'attraction.tag')
+    .join('city', {'city.id': 'attraction.city_id'})
+    .join('attraction_image', {'attraction_image.attraction_id': 'attraction.id',
+      'attraction_image.is_cover_page': knex.raw('?', [true])})
+    .select('attraction.id', 'attraction.name', 'attraction.description',
+      'attraction.tag', 'attraction.alias', 'attraction.visit_hours as visitHours',
+      'attraction.traffic_hours as trafficHours', 'attraction.city_id as cityId',
+      'city.name as cityName', 'attraction_image.image_url as imageUrl',
+      'attraction.nearby_attractions as nearbyAttractions');
 
 const getAttractionByCityName = (cityName) =>
   Attraction()
-    .join('city', {'attraction.city_id': 'city.id'})
-    .select('attraction.id', 'attraction.name', 'attraction.city_id as cityId','city.name as cityName',
-      'attraction.description', 'attraction.alias', 'attraction.tag')
+    .join('city', {'city.id': 'attraction.city_id'})
+    .join('attraction_image', {'attraction_image.attraction_id': 'attraction.id',
+      'attraction_image.is_cover_page': knex.raw('?', [true])})
+    .select('attraction.id', 'attraction.name', 'attraction.description',
+      'attraction.tag', 'attraction.alias', 'attraction.visit_hours as visitHours',
+      'attraction.traffic_hours as trafficHours', 'attraction.city_id as cityId',
+      'city.name as cityName', 'attraction_image.image_url as imageUrl',
+      'attraction.nearby_attractions as nearbyAttractions')
     .where('city.name', cityName);
 
 const getAttractionByCityId = (cityId) =>
   Attraction()
-    .join('city', {'attraction.city_id': 'city.id'})
-    .select('attraction.id', 'attraction.name', 'attraction.city_id as cityId','city.name as cityName',
-      'attraction.description', 'attraction.alias', 'attraction.tag')
+  .join('city', {'city.id': 'attraction.city_id'})
+  .join('attraction_image', {'attraction_image.attraction_id': 'attraction.id',
+    'attraction_image.is_cover_page': knex.raw('?', [true])})
+  .select('attraction.id', 'attraction.name', 'attraction.description',
+    'attraction.tag', 'attraction.alias', 'attraction.visit_hours as visitHours',
+    'attraction.traffic_hours as trafficHours', 'attraction.city_id as cityId',
+    'city.name as cityName', 'attraction_image.image_url as imageUrl',
+    'attraction.nearby_attractions as nearbyAttractions')
     .where('attraction.city_id', cityId);
 
 const getAttraction = (attractionId) =>
