f0b4807e324ba454502b2ce11df7b6d376eaa865
David Xia <david.xia83@gmai.com>
2018-12-01 10:34:55 +1100

    up

--- client/itinerary-item.js
+++ client/itinerary-item.js
@@ -1,12 +1,25 @@
 import React, {createElement} from 'react';
 //import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
 import _ from 'lodash';
+import TagList from './tag-list.js';
 
 // Filter out selected attractions
 const getUnselected = (items, selected) => {
   return _.filter(items, (item) => {
     return !_.find(selected, {attractionId: item.attractionId});
   });
+};
+
+const getTagSetting = (attractions) => {
+  const tags = attractions.map((a) => {
+    return {id: a.attractionId, text: a.name};
+  });
+
+  return {
+    tags: tags,
+    title: null,
+    isReadonly: true,
+  };
 };
 
 export default class ItineraryItem extends React.Component {
@@ -47,20 +60,10 @@ export default class ItineraryItem extends React.Component {
       );
     }
 
-    // Generate html for selected items
-    const elSelected = itinerary.attractions
-      .map((item) => {
-        return (
-          <div className='dnd-item' key={item.id}>
-          {item.name}
-          </div>
-        );
-      });
-
     return (
       <div>
         <div className='dnd-container'>
-          {elSelected}
+          <TagList {...getTagSetting(itinerary.attractions)} />
         </div>
       </div>
     );
