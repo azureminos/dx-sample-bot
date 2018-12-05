1aec891aa1aa3f1cf89fff72221fbad47e53a9e7
David Xia <david.xia83@gmai.com>
2018-12-05 20:30:36 +1100

    up

--- client/app2.jsx
+++ client/app2.jsx
@@ -133,7 +133,8 @@ export default class App2 extends React.Component {
       const liked = u[0].likedAttractions.split(',');
       _.forEach(_.values(cityAttractions), (attrs) => {
         _.forEach(attrs, (attr) => {
-          attr.isLiked = !!_.find(liked, (likedId) => { return likedId===attr.id});
+          console.log('>>>>check liked', {liked: liked, attr: attr});
+          attr.isLiked = !!_.find(liked, (likedId) => { return likedId === attr.id;});
         });
       });
     }
