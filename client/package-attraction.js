4cbf6aff35178ca0f9d129f20dd055d29225f3db
David Xia <david.xia83@gmai.com>
2018-12-12 13:18:59 +1100

    up

--- client/package-attraction.js
+++ client/package-attraction.js
@@ -8,19 +8,19 @@ import CardSlider from './card-slider.jsx';
 import TagList from './tag-list.js';
 import DescPanel from './components/description-panel';
 
-const styles = {
+/*const styles = {
   root: {
     border: '1px solid',
     borderColor: 'lightgrey',
     padding: '4px',
     margin: '4px',
   },
-};
+};*/
 
-class PackageSummary extends React.Component {
+class PackageAttraction extends React.Component {
   render() {
-    console.log('>>>>PackageSummary props', this.props);
-    const {classes, apiUri, cities, cityAttractions, likeAttractions} = this.props;
+    console.log('>>>>PackageAttraction props', this.props);
+    const {apiUri, cities, cityAttractions, likeAttractions} = this.props;
     const citySections = _.keys(cityAttractions).map((city) => {
       const tmpCity = _.find(cities, (c) => {return c.name == city;});
       const cityDesc = !!tmpCity ? tmpCity.description : '';
@@ -49,7 +49,7 @@ class PackageSummary extends React.Component {
       });
 
       return (
-        <div key={cityAttractions[city].id} className={classes.root}>
+        <div key={cityAttractions[city].id}>
           <Typography variant='h5' gutterBottom>
             {city}
           </Typography>
@@ -68,4 +68,4 @@ class PackageSummary extends React.Component {
   }
 }
 
-export default withStyles(styles)(PackageSummary);
+export default PackageAttraction;
