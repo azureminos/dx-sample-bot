5576806043a71746a7e78f5522e1af99ab3c4f30
David Xia <david.xia83@gmai.com>
2018-12-28 03:26:22 +1100

    up

--- messenger-api-helpers/messages.js
+++ messenger-api-helpers/messages.js
@@ -221,7 +221,7 @@ const sharePackageMessage = (apiUri, instId, items) => {
   console.log('>>>>sharePackageMessage(), items grouped by day', dayItems);
   const itinerary = [];
   _.forEach(Object.keys(dayItems), (key) => {
-    if(itinerary.length == 0) {
+    if (itinerary.length !== -1) {
       console.log('>>>>sharePackageMessage(), looping through every day', key);
       const it = dayItems[key];
       itinerary.push(
