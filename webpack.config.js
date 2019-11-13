e22bdda6ae46d7bb3f37ea99806da047690a5b15
apolo2david@yahoo.com <apolo2david@yahoo.com>
2019-11-14 04:17:28 +1100

    getFullItinerary

--- webpack.config.js
+++ webpack.config.js
@@ -5,34 +5,36 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-var path = require('path');
+const path = require('path');
 
-module.exports = [{
-  context: path.join(__dirname),
-  entry: 'index',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+module.exports = [
+  {
+    context: path.join(__dirname),
+    entry: 'index',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js',
+    },
+    optimization: {
+      minimize: false,
+    },
+    module: {
+      rules: [
+        {test: /\.css$/, loader: 'style-loader!css-loader?-svgo'},
+        {
+          test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+          loader: 'url-loader',
+        },
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      modules: [path.join(__dirname, 'client'), 'node_modules'],
+    },
   },
-  module: {
-    rules: [
-      { test: /\.css$/, loader: 'style-loader!css-loader?-svgo' },
-      {
-        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-        loader: 'url-loader',
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    modules: [
-      path.join(__dirname, 'client'),
-      'node_modules'
-    ]
-  }
-}];
+];
