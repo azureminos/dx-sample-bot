f12e4ddb3e0bd0bcc73279448fa5a9eb61207378
apolo2david@yahoo.com <apolo2david@yahoo.com>
2021-01-10 05:32:35 +1100

    up

--- client/pages/pg-select-dest.js
+++ client/pages/pg-select-dest.js
@@ -1,12 +1,15 @@
 import _ from 'lodash';
 import React, {createElement} from 'react';
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  IconButton,
-} from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 import SearchIcon from '@material-ui/icons/Search';
 
 import {fade, withStyles} from '@material-ui/core/styles';
@@ -35,6 +38,24 @@ const styles = (theme) => ({
     padding: 0,
     minHeight: '16px',
   },
+  cityGrid: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  cityGridList: {
+    flexWrap: 'nowrap',
+    transform: 'translateZ(0)',
+  },
+  cityTitle: {
+    color: theme.palette.primary.light,
+  },
+  cityTitleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
 });
 
 class PageSelectDest extends React.Component {
@@ -49,8 +70,31 @@ class PageSelectDest extends React.Component {
     // Sub Components
     let body = <div>Empty destination list</div>;
     if (reference.destinations && reference.destinations.length > 0) {
-      const getDestItem = (d) => {
-        return <div key={d.destinationId}>{d.name}</div>;
+      const getCityGrid = (c) => {
+        return (
+          <GridListTile key={c.destinationId}>
+            <div>
+              <img
+                src={
+                  'https://www.eainsurance.com.au/wp-content/uploads/2020/05/Travel1.jpg'
+                }
+                alt={c.name}
+              />
+              <div>{c.name}</div>
+            </div>
+            <GridListTileBar
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
+              actionIcon={
+                <IconButton aria-label={`star ${c.title}`}>
+                  <StarBorderIcon className={classes.title} />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        );
       };
       body = (
         <div>
@@ -60,7 +104,17 @@ class PageSelectDest extends React.Component {
             }`}</div>
             <div>{cities.length > 0 ? cities.toString() : ''}</div>
           </div>
-          <div>{_.map(reference.destinations, getDestItem)}</div>
+          <div className={classes.root}>
+            <GridList
+              cellHeight={180}
+              className={classes.cityGridList}
+              cols={1}
+            >
+              {reference.destinations.map(getCityGrid)}
+            </GridList>
+          </div>
+
+          <div>{_.map(reference.destinations, getCityGrid)}</div>
         </div>
       );
     }
