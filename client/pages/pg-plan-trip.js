ed772872d189033b141feec3e68eea0d2d6f3d49
apolo2david@yahoo.com <apolo2david@yahoo.com>
2021-01-15 00:22:35 +1100

    up

--- client/pages/pg-plan-trip.js
+++ client/pages/pg-plan-trip.js
@@ -1,139 +1,74 @@
 import _ from 'lodash';
 import React, {createElement} from 'react';
-import {withStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import PackageSummary from '../components-v2/package-summary';
-import BotFooter from '../components-v2/bot-footer';
-import Helper from '../../lib/helper';
-import PackageHelper from '../../lib/package-helper';
-import CONSTANTS from '../../lib/constants';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 
-// Functions
-const TabPanel = (props) => {
-  const {children, value, index, ...other} = props;
-  return value === index ? (
-    <Typography
-      component='div'
-      role='tabpanel'
-      id={`scrollable-auto-tabpanel-${index}`}
-      aria-labelledby={`scrollable-auto-tab-${index}`}
-      {...other}
-    >
-      <div>{children}</div>
-    </Typography>
-  ) : (
-    ''
-  );
-};
-const a11yProps = (index) => {
-  return {
-    id: `scrollable-auto-tab-${index}`,
-    'aria-controls': `scrollable-auto-tabpanel-${index}`,
-  };
-};
+import SearchIcon from '@material-ui/icons/Search';
+
+import {withStyles} from '@material-ui/core/styles';
+// ====== Icons ======
 // Variables
 const styles = (theme) => ({
-  root: {
-    flexGrow: 1,
-    width: '100%',
-    backgroundColor: theme.palette.background.paper,
-  },
-  whitespaceTop: {
-    height: 50,
-  },
-  whitespaceBottom: {
-    height: 100,
-  },
-  headerBar: {
+  root: {},
+
+  appBarFooter: {
     position: 'fixed',
     width: '100%',
-    top: 0,
-    bottom: 'auto',
+    top: 'auto',
+    bottom: 0,
   },
-  list: {
-    padding: 8,
-  },
-  listItem: {
-    padding: '8px 0px 8px 0px',
-  },
-  itemText: {
-    fontSize: '1rem',
-    fontWeight: 'bolder',
+  toolbarFooter: {
+    display: 'block',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 0,
+    minHeight: '16px',
   },
 });
+
 class PagePlanTrip extends React.Component {
   constructor(props) {
     super(props);
-    // Bind handler
-    this.doHandleTabSelect = this.doHandleTabSelect.bind(this);
-    // Setup state
-    this.state = {tabSelected: this.props.daySelected || 0};
+    this.state = {};
   }
-  // Event Handlers
-  doHandleTabSelect = (event, newValue) => {
-    console.log('>>>>PagePlanTrip.doHandleTabSelect', newValue);
-    this.setState({tabSelected: newValue});
-  };
-  // Display Widget
+  // Event Handler
+  // Display page
   render() {
-    // Local variables
-    const {classes, plan, planExt, reference, actions} = this.props;
-    const {tabSelected} = this.state;
+    console.log('>>>>PagePlanTrip, render()', this.props);
+    const {classes} = this.props;
+    // Local Variables
+    // Sub Components
+    const body = <div>tabs</div>;
 
-    const footerActions = {
-      handlePeople: null,
-      handleRoom: null,
-      handleShare: null,
-      handlePayment: null,
-    };
-    // Sub Widgets
-    const tabLabels = [
-      <Tab key={'Summary'} label={'Summary'} {...a11yProps(0)} />,
-    ];
-    _.each(plan.days, (it) => {
-      const label = `Day ${it.dayNo}`;
-      tabLabels.push(
-        <Tab key={label} label={label} {...a11yProps(it.dayNo - 1)} />
-      );
-    });
-    const tabPanels = [
-      <TabPanel key={'Summary'} value={tabSelected} index={0}>
-        <PackageSummary />
-      </TabPanel>,
-    ];
-
-    _.each(plan.days, (it) => {
-      tabPanels.push(
-        <TabPanel key={it.dayNo} value={tabSelected} index={it.dayNo}>
-          <div>To be added</div>
-        </TabPanel>
-      );
-    });
+    // Display Widget
     return (
       <div className={classes.root}>
-        <AppBar position='fixed' color='default' className={classes.headerBar}>
-          <Tabs
-            value={tabSelected}
-            onChange={this.doHandleTabSelect}
-            indicatorColor='primary'
-            textColor='primary'
-            variant='scrollable'
-            scrollButtons='auto'
-            aria-label='scrollable auto tabs example'
-          >
-            {tabLabels}
-          </Tabs>
+        <AppBar position='static'>
+          <Toolbar>
+            <div>
+              <div>Date Selector</div>
+              <div>Search</div>
+            </div>
+          </Toolbar>
         </AppBar>
-        <div className={classes.whitespaceTop} />
-        {tabPanels}
-        <div className={classes.whitespaceBottom} />
-        <BotFooter actions={footerActions} />
+        {body}
+        <AppBar
+          position='fixed'
+          color='default'
+          className={classes.appBarFooter}
+        >
+          <Toolbar className={classes.toolbarFooter}>
+            <Button fullWidth color='primary'>
+              Complete
+            </Button>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default withStyles(styles, {withTheme: true})(PagePlanTrip);
+export default withStyles(styles)(PagePlanTrip);
