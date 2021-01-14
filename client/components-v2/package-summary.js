eede9f0151b7f531a448995b34c0907930dc6552
apolo2david@yahoo.com <apolo2david@yahoo.com>
2021-01-14 23:23:19 +1100

    up

--- client/components-v2/package-summary.js
+++ client/components-v2/package-summary.js
@@ -2,57 +2,12 @@ import _ from 'lodash';
 import React, {createElement} from 'react';
 import {Element, scroller} from 'react-scroll';
 import {withStyles} from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import Typography from '@material-ui/core/Typography';
-import packageHelper from '../../lib/package-helper';
-import CONSTANTS from '../../lib/constants';
 // ====== Icons ======
 // Variables
 const styles = (theme) => ({
   root: {
     width: '100%',
     backgroundColor: theme.palette.background.paper,
-  },
-  itinerary: {
-    display: 'block',
-  },
-  figure: {
-    marginInlineStart: 0,
-    marginInlineEnd: 0,
-    cursor: 'pointer',
-  },
-  imageWrapper: {
-    display: 'block',
-    height: 0,
-    paddingBottom: '100%',
-  },
-  image: {
-    width: '100%',
-    border: 0,
-    display: 'block',
-    maxWidth: '100%',
-  },
-  captionWrapper: {
-    height: '100%',
-    left: 0,
-    padding: '20px 15px',
-    position: 'absolute',
-    top: '100%',
-    width: '100%',
-  },
-  caption: {
-    bottom: '100%',
-    color: '#fff',
-    left: 15,
-    marginTop: 0,
-    marginBottom: 20,
-    position: 'absolute',
-    right: 15,
-    fontWeight: 'bold',
   },
 });
 
@@ -63,85 +18,23 @@ class PackageSummary extends React.Component {
     // Init data
     // Setup state
   }
-  componentDidMount() {
+  /* componentDidMount() {
     scroller.scrollTo('myScrollToElement', {
       duration: 0,
       delay: 0,
       smooth: 'easeInOutQuart',
       offset: -50,
     });
-  }
+  }*/
   // Event Handlers
   // Display Widget
   render() {
-    const {handleSelectCar, handleSelectFlight, handleClickDay} = this.props;
-    const {classes, transport, itineraries, cities} = this.props;
-    const {startDate, carOption} = transport;
-    const {totalDays, departDates, carOptions} = transport;
-    console.log('>>>>PackageSummary, render()', {
-      transport,
-      itineraries,
-      cities,
-    });
+    const {classes, plan, planExt, reference, actions} = this.props;
+    console.log('>>>>PackageSummary, render()', {});
     // Local Variables
     // Sub Components
-    const secFlightCar = (
-      <FlightCar
-        totalDays={totalDays}
-        departDates={departDates}
-        carOptions={carOptions}
-        startDate={startDate}
-        carOption={carOption}
-        handleSelectCar={handleSelectCar}
-        handleSelectFlight={handleSelectFlight}
-      />
-    );
-    const divDays = _.map(itineraries, (it) => {
-      const labelItinerary = `Day ${it.dayNo}, ${it.cityVisit}`;
-      const divLabelItinerary =
-        it.dayNo === 1 ? (
-          <Element name='myScrollToElement'>{labelItinerary}</Element>
-        ) : (
-          labelItinerary
-        );
-      const cityImageUrl = packageHelper.getCityImage(it, cities) || '';
-      return (
-        <ListItem key={it.dayNo} className={classes.itinerary}>
-          <GridList cellHeight={160} className={classes.gridList} cols={1}>
-            <GridListTile
-              cols={1}
-              onClick={() => {
-                if (handleClickDay) {
-                  handleClickDay({}, it.dayNo);
-                }
-              }}
-            >
-              <figure className={classes.figure}>
-                <span className={classes.imageWrapper}>
-                  <img
-                    src={cityImageUrl}
-                    alt={labelItinerary}
-                    className={classes.image}
-                  />
-                </span>
-                <figcaption className={classes.captionWrapper}>
-                  <h4 className={classes.caption}>{divLabelItinerary}</h4>
-                </figcaption>
-              </figure>
-            </GridListTile>
-          </GridList>
-        </ListItem>
-      );
-    });
     // Display Widget
-    return (
-      <List className={classes.root}>
-        <ListItem key={'date-selector-1'}>{secFlightCar}</ListItem>
-        <Divider />
-        {divDays}
-        <Divider />
-      </List>
-    );
+    return <div>trip plan</div>;
   }
 }
 
