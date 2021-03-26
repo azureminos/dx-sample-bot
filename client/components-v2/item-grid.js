7376de543f1ea3d132e5be95add6a8f5a764e437
apolo2david@yahoo.com <apolo2david@yahoo.com>
2021-03-26 14:28:11 +1100

    up

--- client/components-v2/item-grid.js
+++ client/components-v2/item-grid.js
@@ -1,10 +1,13 @@
 // Components
+import _ from 'lodash';
 import React, {createElement} from 'react';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 import Popover from '@material-ui/core/Popover';
 import {withStyles} from '@material-ui/core/styles';
 import CONSTANTS from '../../lib/constants';
-
+import AddBoxIcon from '@material-ui/icons/AddBoxOutlined';
+import MinusBoxIcon from '@material-ui/icons/IndeterminateCheckBoxOutlined';
 // Styles
 // Variables
 const {ATTRACTION, PRODUCT} = CONSTANTS.get().DataModel.TravelPlanItemType;
@@ -15,6 +18,18 @@ const styles = (theme) => ({
   image: {
     width: '-webkit-fill-available',
   },
+  hDivFlex: {
+    display: 'flex',
+    margin: 'auto',
+  },
+  hDivPeopleDisplay: {
+    padding: '4px',
+    margin: 'auto',
+  },
+  hDivPeopleControl: {
+    minWidth: '32px',
+    padding: '4px',
+  },
 });
 
 class ItemGrid extends React.Component {
@@ -23,6 +38,8 @@ class ItemGrid extends React.Component {
     // Bind event handlers
     this.handleClickTraveler = this.handleClickTraveler.bind(this);
     this.handleCloseTraveler = this.handleCloseTraveler.bind(this);
+    this.doHandleAdultChange = this.doHandleAdultChange.bind(this);
+    this.doHandleKidChange = this.doHandleKidChange.bind(this);
     // Init data
     // Setup state
     this.state = {
@@ -40,12 +57,28 @@ class ItemGrid extends React.Component {
   handleCloseTraveler() {
     this.setState({anchorTraveler: null, widthTraveler: null});
   }
+  doHandleAdultChange(input) {
+    // console.log('>>>>ItemGrid.doHandleAdultChange', input);
+    const {actions} = this.props;
+    if (actions && actions.handleAdultChange) {
+      actions.handleAdultChange(input);
+    }
+  }
+  doHandleKidChange(input) {
+    // console.log('>>>>ItemGrid.doHandleKidChange', input);
+    const {actions} = this.props;
+    if (actions && actions.handleKidChange) {
+      actions.handleKidChange(input);
+    }
+  }
   // Display Widget
   render() {
     // console.log('>>>>ItemGrid.render', this.props);
     // Local Variables
     const {classes, item, maxPeople, reference, actions} = this.props;
     const {anchorTraveler, widthTraveler} = this.state;
+    const {activities} = reference;
+    const {attractions, products} = activities[item.destName];
     // Local Functions
     const getImage = () => {
       return (
@@ -59,6 +92,67 @@ class ItemGrid extends React.Component {
         !item.totalAdults && !item.totalKids
           ? 'Number of travelers'
           : `Adults: ${item.totalAdults}, Kids: ${item.totalKids}`;
+      const getButtons = (width) => {
+        const itemExt =
+          item.itemType === PRODUCT
+            ? _.find(products, (p) => {
+              return p.productCode === item.itemId;
+            })
+            : _.find(attractions, (a) => {
+              return a.seoId === item.itemId;
+            });
+        if (item.pricePlan) {
+          return (
+            <div style={{width: `${width || 100}px`}}>price plan options</div>
+          );
+        }
+        return (
+          <div style={{width: `${width || 100}px`}}>
+            <div className={classes.hDivFlex}>
+              <div className={classes.hDivPeopleDisplay}>Adult</div>
+              <IconButton
+                onClick={() => {
+                  this.doHandleAdultChange(1);
+                }}
+                className={classes.hDivPeopleControl}
+              >
+                <AddBoxIcon color='primary' fontSize='default' />
+              </IconButton>
+              <div className={classes.hDivPeopleDisplay}>
+                {item.totalAdults}
+              </div>
+              <IconButton
+                onClick={() => {
+                  this.doHandleAdultChange(-1);
+                }}
+                className={classes.hDivPeopleControl}
+              >
+                <MinusBoxIcon color='primary' fontSize='default' />
+              </IconButton>
+            </div>
+            <div className={classes.hDivFlex}>
+              <div className={classes.hDivPeopleDisplay}>Kid</div>
+              <IconButton
+                onClick={() => {
+                  this.doHandleKidChange(1);
+                }}
+                className={classes.hDivPeopleControl}
+              >
+                <AddBoxIcon color='primary' fontSize='default' />
+              </IconButton>
+              <div className={classes.hDivPeopleDisplay}>{item.totalKids}</div>
+              <IconButton
+                onClick={() => {
+                  this.doHandleKidChange(-1);
+                }}
+                className={classes.hDivPeopleControl}
+              >
+                <MinusBoxIcon color='primary' fontSize='default' />
+              </IconButton>
+            </div>
+          </div>
+        );
+      };
       return (
         <div>
           <Button
@@ -82,9 +176,7 @@ class ItemGrid extends React.Component {
               horizontal: 'left',
             }}
           >
-            <div style={{width: `${widthTraveler || 100}px`}}>
-              The content of the Popover.
-            </div>
+            {getButtons(widthTraveler)}
           </Popover>
         </div>
       );
